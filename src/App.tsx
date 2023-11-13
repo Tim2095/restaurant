9da62f0d6c628cf9f8ced1b264631e8349feb000
Timur Mosenkov <timur.mosenkov@gmail.com>
2023-11-13 21:06:59 +0200

    fix error

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Header from "./layout/Header";
 import RootLayout from "./RootLayout";
 import About from "./pages/About";
-import MainSection from "./components/MainSection";
 import Home from "./pages/Home";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <RootLayout />, 
+    path: "/",
+    element: <RootLayout />,
     children: [
       {
         path: "/",
         element: <Home />,
       },
       {
-        path: 'about',
-        element: <About />
-      }
-    ] 
+        path: "about",
+        element: <About />,
+      },
+    ],
   },
- 
 ]);
 
 function App() {
